90821488193f9cca5607ab5abbb98538acb5f137
ALEXGNH <nolasco.hdz.anagabriela.c.2021@gmail.com>
2026-09-19 08:49:52 -0600

    9

--- bento.js
+++ bento.js
@@ -1,28 +1,26 @@
 const board = document.getElementById('box')
 const pieces = document.querySelectorAll('.img')
-const slots = document.querySelectorAll('.slots')
+const slots = document.getElementById('drop1, drop2, drop3, drop4')
 
-pieces.forEch((piece) => {
-    piece.addEeventListener('dragstart', (e) => {
-        e.dataTransfer.setData('text/plaim', e.target.id);
+pieces.forEach((piece) => {
+    piece.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData('text/plain', e.target.id);
     });
 });
 
 slots.forEach((slot) => {
-slot.addEventListener ('dragover', (e) => {
+ slot.addEventListener ('dragover', (e) => {
     e.preventDefault();
-
 });
 slot.addEventListener('drop', (e) => {
     e.preventDefault();
-
-
-const draggedpieceId = 
-e.dataTransfer.getData('text/plai');
-const draggedpiece = document.getElementById('draggedpieceId');
- if (slot.children.length === 0 && draggedpiece) {
-    slot.appendChild(draggedpiece);
- }
- });
 });
-//breakfast time
+
+  const draggedpieceId = e.dataTransfer.getData('text/plain');
+  const draggedpiece = document.getElementById(draggedpieceId);
+    if (slot.children.length === 0 && draggedpiece) {
+        slot.appendChild(draggedpiece);
+        }
+    });
+
+///im tired ill end this next week w a friends hellp
