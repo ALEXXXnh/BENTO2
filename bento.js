b1014c2c8535012ecdbeae13db7be89c5b01946a
ALEXGNH <nolasco.hdz.anagabriela.c.2021@gmail.com>
2026-09-19 05:47:48 -0600

    7

--- bento.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.querySelector('.cursorshi')
-
- window.addEventListener('mousemove', (e)=> 
-{
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-});
